931a813b2374da35b4bb8b2d8c1ac5164750d805
PradeepKundekar0101 <pradeepkundekargithub@gmail.com>
2026-03-09 12:47:55 +0530

    Update CORS configuration to restrict access to specific origin and enable credentials

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,14 +3,13 @@ import cors from "cors";
 import projectRouter from "./routes/projectRouter.js";
 import authRouter from "./routes/authRouter.js";
 import cookieParser from "cookie-parser";
-import { tr } from "zod/locales";
 import chatRouter from "./routes/chatRoutes.js";
 
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://getbuild.tech", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
